ada75558206da483052cfddd46049993c90bd9df
quddus-larik <qudduslarek@proton.me>
2025-12-30 22:14:00 +0500

    update ui

--- app/page.js
+++ app/page.js
@@ -1,10 +1,25 @@
+"use client"
 
-function Home(){
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
-  return(
+function Home() {
+
+  return (
     <>
-    <h1 className="text-3xl">Hello Abubakar, umair, quddus </h1>
-    <p>Hello I edited Umair's code. Please try to use professional language while commiting.</p>
+      <h1 className="text-3xl">Hello Abubakar, umair, quddus </h1>
+      <p>Hello I edited Umair's code. Please try to use professional language while commiting.</p>
+      <Button onClick={() => alert("i am clicked")}>Click Me</Button>
+      <Popover>
+        <PopoverTrigger>Open</PopoverTrigger>
+        <PopoverContent>
+          <Button>Click</Button>
+        </PopoverContent>
+      </Popover>
     </>
   )
 }
