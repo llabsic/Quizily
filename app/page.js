1845239495eade64fca76dd9f1f7855caeca91a9
devs-abubakar <m.abubakar.devs@gmail.com>
2025-12-30 21:25:47 -0800

    instructed Umair to be professional

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ function Home(){
   return(
     <>
     <h1 className="text-3xl">Hello Abubakar, umair, quddus </h1>
-    <p>Hlo friends kaiso haaaa.</p>
+    <p>Hello I edited Umair's code. Please try to use professional language while commiting.</p>
     </>
   )
 }
