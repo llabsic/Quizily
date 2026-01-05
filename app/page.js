4c9b2d5e95f60966695e9f676abf360d80b5335f
quddus-larik <qudduslarek@proton.me>
2026-01-05 22:19:20 +0500

    modify: linting code

--- app/page.js
+++ app/page.js
@@ -37,7 +37,7 @@ function Home() {
         { answer: "umair khan", isCorrect: false },
       ],
     },
-        {
+    {
       id: "q-4",
       question: "Why's there a bug in the code?",
       options: [
@@ -45,16 +45,19 @@ function Home() {
         { answer: "Didn't know about it", isCorrect: true },
         { answer: "It doesn't matter", isCorrect: false },
       ],
-    },    {
+    },
+    {
       id: "q-5",
       question: "Were u able to find the bug?",
       options: [
         { answer: "No submit btn", isCorrect: false },
-        { answer: "First mcq can't be marked because of material UI", isCorrect: true },
+        {
+          answer: "First mcq can't be marked because of material UI",
+          isCorrect: true,
+        },
         { answer: "Data could have been efficient", isCorrect: false },
       ],
     },
-
   ];
 
   const currentQuestion = quiz[currentIndex];
