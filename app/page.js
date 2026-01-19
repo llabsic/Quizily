779644ea913dadbd48143d059cea628dbf0a9d04
devs-abubakar <m.abubakar.devs@gmail.com>
2026-01-18 21:59:10 -0800

    Added the AI feature to generate quizzes using Gemini API.
    Co-authored-by: QUDDUS <qudduslarek@proton.me>

--- app/page.js
+++ app/page.js
@@ -3,67 +3,105 @@
 import { useState } from "react";
 import { RadioGrp, RadioItm } from "@/components/custom/radio";
 import { Button } from "@/components/ui/button";
+import {useEffect } from "react";
+import { QuizService } from "@/lib/QuizService";
+import QuizLoader from "@/components/custom/QuizLoader";
 
-function Home() {
+export default function Home() {
+  const QuestionTime= 30; // seconds per question
   const [answers, setAnswers] = useState({});
+  const [quiz,setQuiz]=useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [submitted, setSubmitted] = useState(false);
   const [score, setScore] = useState(0);
+  const [timeleft, setTimeleft] = useState(QuestionTime);
+  const [loading,setloading]=useState(false);
+  const topic="General Knowledge";
 
-  const quiz = [
-    {
-      id: "q-1",
-      question: "what is correct spelling of Quddus?",
-      options: [
-        { answer: "quddis", isCorrect: false },
-        { answer: "qudoos", isCorrect: false },
-        { answer: "qidis", isCorrect: false },
-        { answer: "quddus", isCorrect: true },
-      ],
-    },
-    {
-      id: "q-2",
-      question: "What is correct spelling of Abu bakar",
-      options: [
-        { answer: "abu Bakar", isCorrect: false },
-        { answer: "Abu Bakar", isCorrect: true },
-        { answer: "Abu bakar", isCorrect: false },
-      ],
-    },
-    {
-      id: "q-3",
-      question: "What is correct spelling of Umair haha!!!",
-      options: [
-        { answer: "Uamair", isCorrect: false },
-        { answer: "Umair Khan", isCorrect: true },
-        { answer: "umair khan", isCorrect: false },
-      ],
-    },
-    {
-      id: "q-4",
-      question: "Why's there a bug in the code?",
-      options: [
-        { answer: "Never paid attention", isCorrect: false },
-        { answer: "Didn't know about it", isCorrect: true },
-        { answer: "It doesn't matter", isCorrect: false },
-      ],
-    },
-    {
-      id: "q-5",
-      question: "Were u able to find the bug?",
-      options: [
-        { answer: "No submit btn", isCorrect: false },
-        {
-          answer: "First mcq can't be marked because of material UI",
-          isCorrect: true,
-        },
-        { answer: "Data could have been efficient", isCorrect: false },
-      ],
-    },
-  ];
+  useEffect(()=>{
+    handleQuizData(topic);
+  },[])
+
+  // const quiz = [
+  //   {
+  //     id: "q-1",
+  //     question: "what is correct spelling of Quddus?",
+  //     options: [
+  //       { answer: "quddis", isCorrect: false },
+  //       { answer: "qudoos", isCorrect: false },
+  //       { answer: "qidis", isCorrect: false },
+  //       { answer: "quddus", isCorrect: true },
+  //     ],
+  //   },
+  //   {
+  //     id: "q-2",
+  //     question: "What is correct spelling of Abu bakar",
+  //     options: [
+  //       { answer: "abu Bakar", isCorrect: false },
+  //       { answer: "Abu Bakar", isCorrect: true },
+  //       { answer: "Abu bakar", isCorrect: false },
+  //     ],
+  //   },
+  //   {
+  //     id: "q-3",
+  //     question: "What is correct spelling of Umair haha!!!",
+  //     options: [
+  //       { answer: "Uamair", isCorrect: false },
+  //       { answer: "Umair Khan", isCorrect: true },
+  //       { answer: "umair khan", isCorrect: false },
+  //     ],
+  //   },
+  //   {
+  //     id: "q-4",
+  //     question: "Why's there a bug in the code?",
+  //     options: [
+  //       { answer: "Never paid attention", isCorrect: false },
+  //       { answer: "Didn't know about it", isCorrect: true },
+  //       { answer: "It doesn't matter", isCorrect: false },
+  //     ],
+  //   },
+  //   {
+  //     id: "q-5",
+  //     question: "Were u able to find the bug?",
+  //     options: [
+  //       { answer: "No submit btn", isCorrect: false },
+  //       {
+  //         answer: "First mcq can't be marked because of material UI",
+  //         isCorrect: true,
+  //       },
+  //       { answer: "Data could have been efficient", isCorrect: false },
+  //     ],
+  //   },
+  // ];
 
   const currentQuestion = quiz[currentIndex];
-  const selectedAnswer = answers[currentQuestion.id];
+  const selectedAnswer = currentQuestion?answers[currentQuestion.id]:null;
+  useEffect(() => {
+    setTimeleft(QuestionTime);
+
+  const interval = setInterval(() => {
+    setTimeleft((prev) => {
+      if (prev <= 1) {
+        clearInterval(interval);
+        handleNext();
+        return QuestionTime;
+      }
+      return prev - 1;
+    });
+  }, 1000);
+
+  return () => clearInterval(interval);
+}, [currentIndex]);
+
+  const handleQuizData = async(topic)=>{
+    try{
+      setloading(true);
+      const newQuiz= await QuizService.generate(topic);
+      setQuiz(newQuiz);
+
+    }catch(err){
+      console.error("Error fetching quiz:",err);
+    }finally{setloading(false);}}
 
   const handleNext = () => {
     if (currentIndex < quiz.length - 1) {
@@ -79,45 +117,58 @@ function Home() {
 
   const handleSubmit = () => {
     let correct = 0;
-
+    
     quiz.forEach((q) => {
       const selected = answers[q.id];
       const correctAnswer = q.options.find((o) => o.isCorrect)?.answer;
-
+      
       if (selected === correctAnswer) {
         correct++;
       }
     });
-
+    
+    setTimeleft(0);
     setScore(correct);
     setSubmitted(true);
   };
 
   return (
-    <div className="p-3 flex flex-col gap-4 max-w-xl">
+    <div className="w-screen h-screen flex items-center justify-center">
+
+    <div className="p-3 flex flex-col gap-4 rounded-2xl overflow-hidden border-2 shadow-lg w-2/3 ">
       <h1 className="font-google text-lg">Custom Radio UI Quiz</h1>
 
       <div className="flex flex-col gap-3">
-        <h2 className="font-semibold">{currentQuestion.question}</h2>
-
-        <RadioGrp
+        <div className="flex items-center gap-2 p-2 rounded-md justify-between">
+        <h2 className="font-semibold">{currentQuestion?currentQuestion.question:null}</h2>
+        <div className="font-bold">Time:<span className={timeleft<=5?"text-red-600":"text-green-600"}>
+          {timeleft}
+          </span>
+          </div>
+        </div>
+        <div className=" flex w-full items-center">
+      
+        {quiz.length>0? <RadioGrp
           value={selectedAnswer ?? ""}
+
+
           onValueChange={(value) =>
             setAnswers((prev) => ({
               ...prev,
               [currentQuestion.id]: value,
             }))
           }
-        >
+          >
           {currentQuestion.options.map((option, idx) => (
             <RadioItm
-              key={`${currentQuestion.id}-${idx}`}
-              value={option.answer}
+            key={`${currentQuestion.id}-${idx}`}
+            value={option.answer}
             >
               {option.answer}
             </RadioItm>
           ))}
-        </RadioGrp>
+        </RadioGrp> : <QuizLoader />}
+        </div>
       </div>
 
       <div className="flex gap-2 mt-4">
@@ -156,7 +207,8 @@ function Home() {
         </div>
       )}
     </div>
+    </div>
   );
 }
 
-export default Home;
+
