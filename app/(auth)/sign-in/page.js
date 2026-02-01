004ab5e45ff85aa36a0473b4d4dd4bfb5e0c7812
QUDDUS <qudduslarek@proton.me>
2026-02-01 23:54:47 +0500

    initialize authentication

---
+++ app/(auth)/sign-in/page.js
@@ -0,0 +1,23 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useSupabase } from "@/hooks/useSupabase"
+
+export default function Page(){
+    const { supabase } = useSupabase();
+
+    const handleSigIn = () =>{
+        supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                redirectTo: "http://localhost:3000/" // v1/callback
+            }
+        })
+    }
+
+    return(
+        <div className="w-full h-svh grid place-content-center">
+            <Button onClick={handleSigIn}>SignIN</Button>
+        </div>
+    )
+}
---
+++ hooks/useSupabase.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+export function useSupabase(){
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  return { supabase }
+}
