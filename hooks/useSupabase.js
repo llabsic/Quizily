bbeba4f3debf8aa30ea33024a60664aefdd6dc1b
QUDDUS <qudduslarek@proton.me>
2026-02-02 23:32:35 +0500

    delete upwanted data and files

--- hooks/useSupabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-export function useSupabase(){
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-  return { supabase }
-}
