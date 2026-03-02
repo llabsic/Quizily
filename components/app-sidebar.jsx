62a04b79a5d08980a4dc762d641a6d4da04bcd82
QUDDUS <qudduslarek@proton.me>
2026-03-02 19:32:17 +0500

    chore: dashboard initialization

---
+++ components/app-sidebar.jsx
@@ -0,0 +1,143 @@
+"use client";
+
+import * as React from "react";
+import {
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  LifeBuoy,
+  Map,
+  PieChart,
+  Send,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavSecondary } from "@/components/nav-secondary";
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { useEffect, useState } from "react";
+import { authUser } from "@/lib/supabase";
+import { History } from "lucide-react";
+import { BookCopy } from "lucide-react";
+import { Settings } from "lucide-react";
+
+export function AppSidebar({ ...props }) {
+  const initialData = {
+    user: {
+      name: "lixy",
+      email: "lixy@example.com",
+      avatar: "/avatars/shadcn.jpg",
+    },
+    navMain: [
+      {
+        title: "Courses",
+        url: "/dashboard/courses",
+        icon: BookCopy,
+        isActive: true,
+      },
+      {
+        title: "History",
+        url: "/dashboard/history",
+        icon: History,
+      },
+      {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+      },
+    ],
+    navSecondary: [
+      {
+        title: "Support",
+        url: "/company/support",
+        icon: LifeBuoy,
+      },
+      {
+        title: "Feedback",
+        url: "/company/feedback",
+        icon: Send,
+      },
+    ],
+    projects: [
+      {
+        name: "Design Engineering",
+        url: "#",
+        icon: Frame,
+      },
+      {
+        name: "Sales & Marketing",
+        url: "#",
+        icon: PieChart,
+      },
+      {
+        name: "Travel",
+        url: "#",
+        icon: Map,
+      },
+    ],
+  };
+
+  const [data, setData] = useState(initialData);
+
+  useEffect(() => {
+  (async () => {
+    const userData = await authUser();
+
+    const email = userData?.data?.session?.user?.email;
+    const user = userData?.data?.session?.user.user_metadata.full_name;
+    const avatar = userData?.data?.session?.user.user_metadata.avatar_url;
+
+    if (email) {
+      setData((prev) => ({
+        ...prev,
+        user: {
+          ...prev.user,
+          email,
+          name: user,
+          avatar
+        },
+      }));
+    }
+  })();
+}, []);
+
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="/">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Quizily</span>
+                  <span className="truncate text-xs">lixril</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        { data ? <NavUser user={data.user} /> : null }
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
