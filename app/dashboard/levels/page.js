b51a725a506bcfae438dca84ec74076777d07985
QUDDUS <qudduslarek@proton.me>
2026-03-06 13:14:00 +0500

    rename 'courses to levels'

--- app/dashboard/levels/page.js
+++ app/dashboard/levels/page.js
@@ -1,13 +1,15 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader } from "@/components/ui/card";
+import { ArrowUpRight } from "lucide-react";
 
 export default function Page() {
   const courses = [
     {
       title: "English",
-      description: "Take Quiz For English.",
+      description: "Take Quiz based on English gramer.",
       image: "",
+      keywords: ["Pentuation", "Clauses", "Paricipals"],
     },
     {
       title: "Physics",
@@ -28,13 +30,16 @@ export default function Page() {
   return (
     <div className="grid grid-cols-3 gap-3">
       {courses.map((itm, idx) => (
-        <div className="border border-muted rounded-lg p-2">
-          <div className="bg-linear-to-tr from-violet-600 to-slate-900 rounded-lg h-40 p-1">
-            <Badge className="ml-0.5">{itm.title}</Badge>
+        <div className="border-2 border-muted rounded-lg p-2 cursor-pointer hover:border-muted-foreground transition-colors">
+          <div className="w-full flex items-center justify-end h-4">
+            <ArrowUpRight className="size-4" />
           </div>
-          <div>
-          <p className="text-sm mt-1">{itm.description}</p>
-          <Button className="w-full">Take Test</Button>
+          <span className="text-xl">{itm.title}</span>
+          <p className="text-sm">{itm.description}</p>
+          <div className="flex flex-row gap-1 flex-wrap ml-1 mt-3 mb-4">
+            {itm?.keywords?.map((itm, idx) => (
+              <Badge key={String(itm + idx)}>{itm}</Badge>
+            ))}
           </div>
         </div>
       ))}
