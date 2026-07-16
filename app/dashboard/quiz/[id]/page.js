19301db39407fd6b93776d0e45d1ee86d0bfffd6
QUDDUS <qudduslarek@proton.me>
2026-07-16 10:40:52 +0500

    refactor: update time

--- app/dashboard/quiz/[id]/page.js
+++ app/dashboard/quiz/[id]/page.js
@@ -3,8 +3,8 @@ import QuizBlock from "@/components/custom/quiz-component";
 export default function Page() {
 
     return (
-        <div className={"flex h-full w-full items-start pt-6 justify-start"}>
-            <QuizBlock QuestionTime={10000}/>
+        <div className={"flex w-full items-start pt-6 justify-start"}>
+            <QuizBlock QuestionTime={500}/>
         </div>
     )
 }
