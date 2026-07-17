31b5bac0fb97374af06f92fd285dcef103e1cf19
QUDDUS <qudduslarek@proton.me>
2026-07-17 14:33:43 +0500

    refactor: the main page for log signed user for temp

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
 "use client";
 
+import { supabase } from "@/lib/supabase/client";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+      setUser(session?.user || null);
+    };
+    getSession();
+  }, []);
+
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
-      {/* <QuizBlock /> */}
+      <p>{JSON.stringify(user)}</p>
     </div>
   );
 }
