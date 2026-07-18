7c6b9c4af4229432359b825404bd36274a07d40b
QUDDUS <qudduslarek@proton.me>
2026-07-18 15:57:26 +0500

    mock: data for profile and its components

---
+++ config/profile.js
@@ -0,0 +1,42 @@
+export const Profile = {
+    id: 1,
+    username: "@qudduslarik",
+    displayName: "Quddus",
+    avatarUrl: "https://imgs..",
+    coverUrl: "https://imgs..",
+    currentStreaks: 12,
+    maxStreaks: 34,
+    streaks: [1,1,1,1,0,0,0],
+    quizzesWon: 90,
+    quizzesLost: 82,
+    retries: 12,
+    matchesPlayed: 125,
+
+    ranks: [
+        {
+            date: "12-1-2025",
+            title: "Primeval",
+            season: "2025",
+            lore: "A 2025 legacy hero of Quizily."
+        }
+    ],
+    xp: 586,
+    achievements: [
+        {
+            name: "Correct 10 out of 10 mcqs",
+            unlockedAt: "12-1-2025",
+            rewards: [
+                {
+                    gold: 10,
+                    xp: 100
+                }
+            ]
+        }
+    ],
+    badges: [
+        {
+            name: "Spark Badge",
+            description: "You earned first Spark Badge since 12-1-2025",
+        }
+    ]
+}
