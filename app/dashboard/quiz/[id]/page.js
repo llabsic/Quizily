ac71a6cd5baf05cec87ee1af059dd9249f22250b
QUDDUS <qudduslarek@proton.me>
2026-08-01 19:42:02 +0500

    refactor code

--- app/dashboard/quiz/[id]/page.js
+++ app/dashboard/quiz/[id]/page.js
@@ -1,31 +1,39 @@
-"use client"
+"use client";
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "next/navigation";
 import QuizBlock from "@/components/custom/quiz-component";
 import QuizLoader from "@/components/custom/quiz-loader";
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {useParams} from "next/navigation";
-import {generateQuiz} from "@/stores/slices/quiz.slice";
+import { generateQuiz } from "@/stores/slices/quiz.slice";
 
 export default function Page() {
-    const quizes = useSelector((state) => state.quiz.quizes);
-    const loading = useSelector((state) => state.quiz.loading);
-    const error = useSelector((state) => state.quiz.error);
+    const { quizes, loading, error } = useSelector((state) => state.quiz);
     const dispatch = useDispatch();
     const { id } = useParams();
 
     useEffect(() => {
         if (id) {
-            const topic = "basics html";
-            dispatch(generateQuiz({ topic, totalMcqs: 5 }));
+            dispatch(generateQuiz({ topic: "basics html", totalMcqs: 5 }));
         }
-    }, [dispatch, id]);
+    }, [id, dispatch]);
 
     if (loading) return <QuizLoader />;
-    if (error) return <div className="text-center py-8 text-danger">{error}</div>;
+
+    if (error) {
+        return (
+            <div className="flex w-full items-center justify-center p-8">
+                <div className="rounded-lg border border-danger/20 bg-danger/10 p-4 text-center text-danger">
+                    <p className="font-semibold">Failed to load quiz</p>
+                    <p className="text-sm mt-1">{error}</p>
+                </div>
+            </div>
+        );
+    }
 
     return (
-        <div className={"flex w-full items-start pt-6 justify-start"}>
-            <QuizBlock questions={quizes} QuestionTime={500}/>
+        <div className="flex w-full items-start justify-start pt-6">
+            <QuizBlock questions={quizes} QuestionTime={500} />
         </div>
-    )
+    );
 }
